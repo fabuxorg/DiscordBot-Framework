999dd39c46807c0ce1da4d4450523ff16b40f6ac
Johannes Losch <dev@hannesgames.de>
2019-08-24 14:56:55 +0200

    added UserEvents #2

---
+++ src/main/java/com/johanneslosch/discordbotframework/event/user/UserTyping.java
@@ -0,0 +1,10 @@
+package com.johanneslosch.discordbotframework.event.user;
+
+import net.dv8tion.jda.api.events.user.UserTypingEvent;
+
+import javax.annotation.Nonnull;
+
+public class UserTyping {
+    public void onUserTyping(@Nonnull UserTypingEvent event) {}
+
+}
