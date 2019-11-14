41ef745eaecd5ad54bf5f6adab8e88dec9e10aac
Johannes Losch <hi@johanneslosch.com>
2019-11-14 17:39:21 +0100

    Moved ICommand for rewrite and fixed Typo for #13

--- src/main/java/wf/key/discordbotframework/rewrite/command/ICommand.java
+++ src/main/java/wf/key/discordbotframework/rewrite/command/ICommand.java
@@ -1,10 +1,10 @@
-package com.johanneslosch.discordbotframework.command;
+package wf.key.discordbotframework.rewrite.command;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public interface ICommand {
     boolean called(String[] args, MessageReceivedEvent event);
     void action(String[] args, MessageReceivedEvent event);
-    void executed(boolean sucess, MessageReceivedEvent event);
+    void executed(boolean success, MessageReceivedEvent event);
     String help();
 }
