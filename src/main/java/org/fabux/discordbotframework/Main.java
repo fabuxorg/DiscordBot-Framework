e7187fa8720233283fe998d00eed0819b8b093bb
Johannes Losch <hi@johanneslosch.com>
2019-12-22 16:55:11 +0100

    #25 comment = error

--- src/main/java/org/fabux/discordbotframework/Main.java
+++ src/main/java/org/fabux/discordbotframework/Main.java
@@ -7,7 +7,7 @@ import org.fabux.discordbotframework.util.time.Time;
 
 public class Main {
     public static void main (String [] args){
-        FileHelper.checkFile("logs", String.format("LOG_1"), "log");
+        //FileHelper.checkFile("logs", String.format("LOG_1"), "log");
         //Print "test" in log file
         Logger.logger("test");
 
