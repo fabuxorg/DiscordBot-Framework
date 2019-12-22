2935b6a6b4d1b490f22af5dd0eb6ace9a50e2f93
Johannes Losch <hi@johanneslosch.com>
2019-12-22 17:12:24 +0100

    fixed commit

--- src/main/java/org/fabux/discordbotframework/Main.java
+++ src/main/java/org/fabux/discordbotframework/Main.java
@@ -2,6 +2,7 @@ package org.fabux.discordbotframework;
 
 
 import org.fabux.discordbotframework.util.logs.Logger;
+import org.fabux.discordbotframework.util.versionhandler.VersionHandler;
 
 public class Main {
     public static void main (String [] args){
@@ -9,7 +10,7 @@ public class Main {
         Logger.logger("test");
 
         //Test version
-        VersionHandler.version("fabuxorg", "DiscordBot-Framework");
+        //VersionHandler.version("fabuxorg", "DiscordBot-Framework");
 
     }
 }
