b375121ade4a14e0d2bcb246b6fca68692e1460e
Johannes Losch <hi@johanneslosch.com>
2019-12-23 14:47:13 +0100

    added setup to main

--- src/main/java/org/fabux/discordbotframework/Main.java
+++ src/main/java/org/fabux/discordbotframework/Main.java
@@ -2,12 +2,14 @@ package org.fabux.discordbotframework;
 
 
 import org.fabux.discordbotframework.util.logs.Logger;
-import org.fabux.discordbotframework.util.versionhandler.VersionHandler;
+import org.fabux.discordbotframework.util.setup.Setup;
 
 public class Main {
     public static void main (String [] args){
         //Print "test" in log file
         Logger.logger("test");
+
+        Setup.setup();
 
         //Test version
         //VersionHandler.version("fabuxorg", "DiscordBot-Framework");
