047fd2c98b5894ce3572ea55e8724a1513723c30
johanneslosch <hi@johanneslosch.com>
2020-03-24 19:59:39 +0100

    added custom GUI port

--- src/main/java/org/fabux/discordbotframework/util/gui/webserver/HTTPServer.java
+++ src/main/java/org/fabux/discordbotframework/util/gui/webserver/HTTPServer.java
@@ -25,7 +25,7 @@ public class HTTPServer implements Runnable{
     static final String FILE_NOT_FOUND = "404.html";
     static final String METHOD_NOT_SUPPORTED = "not_supported.html";
     // port to listen connection
-    static final int PORT = 8080;
+    static final int PORT = Integer.parseInt(ConfigReader.read("data", "config", "online_gui_port"));
 
     // verbose mode
     static final boolean verbose = true;
